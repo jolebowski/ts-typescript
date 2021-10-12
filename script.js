f6975283a64116bbfed956dfb5ce5d0f09874dd3
jolebowski <jordan.moreira@epitech.eu>
2021-10-12 12:03:11 +0200

    type null and type undefined

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-// Installer tyspcript en local avec la commande 
-// npm i -g typescript
-// Utiliser la commande tsc script.ts pour 
-// lancer le typescript et ajouter -w pour voir 
-// les modifications en direct 
-console.log("typescript");
+var value; // Ts number
+console.log(value); /// undefined 
+var value1;
+console.log(value1); // undefined 
+// let value2:null; 
+// console.log(value2); // undefined 
+// Dans le cas d'un undefined qu'on attribue a une variable 
+// on est toujours undefined pareil lorsqu'il sagit de null 
+var value2 = null;
+console.log(value2); // null 
+var value3;
+value3 = null;
+value3 = undefined;
+console.log(value3);
+/**
+ * ! INFORMATION : il faut activer dans le fichier
+ * ! tsconfig --strickNullChecks si on souhaite que le type de value3
+ * ! soit bien respecter ou sinon remplacer par unknow ou any
+ */
--- script.ts
+++ script.ts
@@ -1,6 +1,25 @@
-// Installer tyspcript en local avec la commande 
-// npm i -g typescript
-// Utiliser la commande tsc script.ts pour 
-// lancer le typescript et ajouter -w pour voir 
-// les modifications en direct 
-console.log("typescript")
+let value: number // Ts number
+console.log(value); /// undefined 
+
+let value1: undefined; 
+console.log(value1); // undefined 
+
+// let value2:null; 
+// console.log(value2); // undefined 
+
+// Dans le cas d'un undefined qu'on attribue a une variable 
+// on est toujours undefined pareil lorsqu'il sagit de null 
+
+let value2:null = null; 
+console.log(value2); // null 
+
+let value3:string | number; 
+value3 = null;
+value3 = undefined;
+console.log(value3);
+
+/**
+ * ! INFORMATION : il faut activer dans le fichier 
+ * ! tsconfig --strickNullChecks si on souhaite que le type de value3 
+ * ! soit bien respecter ou sinon remplacer par unknow ou any 
+ */
