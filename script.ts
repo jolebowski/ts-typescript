fefe04ef85ad4a62e92d335382984a8b1d34abe7
jolebowski <jordan.moreira@epitech.eu>
2021-10-14 11:30:07 +0200

    edit comment

--- script.ts
+++ script.ts
@@ -1,6 +1,7 @@
-// Installer tyspcript en local avec la commande 
-// npm i -g typescript
-// Utiliser la commande tsc script.ts pour 
-// lancer le typescript et ajouter -w pour voir 
-// les modifications en direct 
+/** 
+ * ? Installer tyspcript en local avec la commande
+ * ? npm i -g typescript 
+ * ? Utiliser la commande tsc script.ts pour lancer le typescript et ajouter -w pour voir
+ * ? les modifications en direct 
+*/
 console.log("typescript")
