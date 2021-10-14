c6673c0190ceb0842e0780d4eb887ed1cfdd2578
jolebowski <jordan.moreira@epitech.eu>
2021-10-14 11:29:27 +0200

    whats typescript

--- script.js
+++ script.js
@@ -3,4 +3,26 @@
 // Utiliser la commande tsc script.ts pour 
 // lancer le typescript et ajouter -w pour voir 
 // les modifications en direct 
-console.log("typescript");
+/**
+ * * Typescript est une extansion de langage qui
+ * * ajoute le typage statique à javascript.
+ *
+ * * Il est open source développé par Miscrosoft et dévenu
+ * * populaire suite à son intégration depuis Angular 2.
+ */
+/**
+ * ! Avec le javascript on lance le code et on constate les éventuels erreurs
+ *
+ * ! Avec le typescript on constate les erreurs avant de lancer le code
+ *
+ */
+/**
+ * * Le code typescript n'est pas reconnu par les navigateurs web
+ *
+ * * Il faut le compiler avec du javascript
+ *
+ * ? Les outils qu'il faut pour utiliser typescript
+ * ? VS code pour intellisense
+ * ? Nodejs pour avoir NPM(gestionnaire de packages) afin de télécharger des packages
+ * ? Navigateur à jour
+ */ 
