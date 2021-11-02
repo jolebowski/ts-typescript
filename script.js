20a843eb622cc534e1ed98b8a019a3beb691d612
jolebowski <jordan.moreira@epitech.eu>
2021-11-02 22:33:11 +0100

    type any

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-// Installer tyspcript en local avec la commande 
-// npm i -g typescript
-// Utiliser la commande tsc script.ts pour 
-// lancer le typescript et ajouter -w pour voir 
-// les modifications en direct 
-console.log("typescript");
+/**
+ * ? Installer tyspcript en local avec la commande
+ * ? npm i -g typescript
+ * ? Utiliser la commande tsc script.ts pour lancer le typescript et ajouter -w pour voir
+ * ? les modifications en direct
+*/
+/**
+ * ? Le type any nous permet simplement de na pas préciser le type
+ * ? via typescript
+ *
+ * * Peut être utile dans certains cas. Ex : On ne sait pas quel type de
+ * * data à récupérer d'une API, d'un formulaire, on souhaite modifier le
+ * * le type etc...
+ */
+var anyData; // any 
+var anyData = [];
--- script.ts
+++ script.ts
@@ -1,7 +1,18 @@
-/** 
+/**
  * ? Installer tyspcript en local avec la commande
- * ? npm i -g typescript 
+ * ? npm i -g typescript
  * ? Utiliser la commande tsc script.ts pour lancer le typescript et ajouter -w pour voir
- * ? les modifications en direct 
-*/
-console.log("typescript")
+ * ? les modifications en direct
+ */
+
+/**
+ * ? Le type any nous permet simplement de na pas préciser le type
+ * ? via typescript
+ *
+ * * Peut être utile dans certains cas. Ex : On ne sait pas quel type de
+ * * data à récupérer d'une API, d'un formulaire, on souhaite modifier le
+ * * le type etc...
+ */
+let anyData; // any
+
+let anyData: any[] = [];
