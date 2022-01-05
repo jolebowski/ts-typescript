929fe39f4227231e5331ba307ea382b72ed131e6
jolebowski <jordan.moreira@epitech.eu>
2022-01-05 10:00:14 +0100

    edit script js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// Installer tyspcript en local avec la commande 
-// npm i -g typescript
-// Utiliser la commande tsc script.ts pour 
-// lancer le typescript et ajouter -w pour voir 
-// les modifications en direct 
-console.log("typescript");
+/**
+ * ? Installer tyspcript en local avec la commande
+ * ? npm i -g typescript
+ * ? Utiliser la commande tsc script.ts pour lancer le typescript et ajouter -w pour voir les modifications en direct
+*/
+console.log("typescript    ");
