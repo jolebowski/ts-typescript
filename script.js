6a80569e99278c6c371731487de62952a1e56cab
jolebowski <jordan.moreira@epitech.eu>
2022-01-05 09:58:06 +0100

    type generic

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-// Installer tyspcript en local avec la commande 
-// npm i -g typescript
-// Utiliser la commande tsc script.ts pour 
-// lancer le typescript et ajouter -w pour voir 
-// les modifications en direct 
-console.log("typescript");
+/**
+ * ? Installer tyspcript en local avec la commande
+ * ? npm i -g typescript
+ * ? Utiliser la commande tsc script.ts pour lancer le typescript et ajouter -w pour voir les modifications en direct
+ */
+console.log("typescript    ");
+var last = function (arr) {
+    return arr[arr.length - 1];
+};
+var l = last([1, 2, 3]);
+var l2 = last(["a", "b", "c"]);
+var makeArr = function (x, y) { return [x, y]; };
+var v = makeArr(1, "okok");
+var v2 = makeArr("ook", true);
