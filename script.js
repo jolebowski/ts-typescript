217b5fe09cc2c1cedc76aa54f13ca12f4c3bd501
jolebowski <jordan.moreira@epitech.eu>
2022-01-06 11:17:50 +0100

    delete code from type generic branch

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
  * ? Installer tyspcript en local avec la commande
  * ? npm i -g typescript
  * ? Utiliser la commande tsc script.ts pour lancer le typescript et ajouter -w pour voir les modifications en direct
-*/
+ */
 console.log("typescript    ");
